224628e8bf48d6d0994c52328826de5100c861ad
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-03-15 10:40:11 +0200

    Create henkilot.sql

---
+++ henkilot.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Henkilot
+(
+	henkilot_henkilo_id integer 		   	  not null primary key,
+	henkilot_etunimi    character varying(40) not null,
+	henkilot_sukunimi	character varying(40) not null,
+	henkilot_email		character varying(100)
+)
