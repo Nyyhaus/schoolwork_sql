65299f0979ed01516b1e04fa7d98c0d8f9af6884
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-03-17 12:21:41 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,3 @@
+insert into kirjat (Kirjat_Kirja_ID, Kirjat_KirjanNimi, Kirjat_Tekija_ID,
+Kirjat_Kustantaja_ID, Kirjat_Julkaisupvm)
+values (110, 'Alavilla mailla hallan vaara', 202, 303, '2002-12-20')
